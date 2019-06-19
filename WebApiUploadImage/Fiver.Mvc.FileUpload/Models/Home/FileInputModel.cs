20bc1c07e0c230893b5e26fb535af5b48de5182a
cybill.netsol@gmail.com <cybill.netsol@gmail.com>
2019-06-19 15:50:03 +0500

    WEB-API Uploading Image Multiple

---
+++ WebApiUploadImage/Fiver.Mvc.FileUpload/Models/Home/FileInputModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace Fiver.Mvc.FileUpload.Models.Home
+{
+    public class FileInputModel
+    {
+        public List<IFormFile> FileToUpload { get; set; }
+    }
+}
